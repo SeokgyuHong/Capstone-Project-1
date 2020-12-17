40996bfd171a1f9ba17d68df3b903f8f57198ae8
SeokgyuHong <58390757+SeokgyuHong@users.noreply.github.com>
2020-12-17 19:21:38 +0900

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -5,7 +5,11 @@ module.exports.data_from_sensor = (req,res)=>{
     let shoes_data = req.body.shoes_data ; //센서로 부터 받은 데이터
     let mac_address = req.body.mac_address;
 
-    const nsp = io.of(mac);
+    io.on('data_from_sensor',(socket)=>{
+        InsertData().then(()=>{
+            io.emit('data_to_sensor',socket)
+        })
+    }
 
-
+    
 }
